b78d82ea2440c3ae07fc0773813084770a4c53a6
DanielArruda98 <arruda.daniel20@gmail.com>
2021-07-13 17:19:01 -0300

    class version 24 - "exbindo usuario na view"

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -25,7 +25,7 @@
 
             <p class="event-owner">
                 <ion-icon name="star-outline"></ion-icon>
-                Dono do Evento
+                {{ $eventOwner['name'] }}
             </p>
 
             <a href="#" class="btn btn-primary" id="event-submit">Confirmar presença</a>
