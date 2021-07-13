2fbf8c14fe26b715fa2cb5e4586c6d1e585f07db
DanielArruda98 <arruda.daniel20@gmail.com>
2021-07-13 15:58:32 -0300

    class version 22 - "autenticação jetstream"

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
