291f7ab49db47128936b811635627bbc6ba555a6
DanielArruda98 <arruda.daniel20@gmail.com>
2021-07-12 22:01:05 -0300

    class version 17 - "upload de imagens"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
     <div id="cards-container" class="row">
         @foreach($events as $event)
             <div class="card col-md-3">
-                <img src="/img/banner.jpeg" alt="{{ $event->title }}">
+                <img src="/img/events/{{ $event->image ? $event->image : '../banner.jpeg' }}" alt="{{ $event->title }}">
 
                 <div class="card-body">
                     <p class="card-date">11/07/2021</p>
